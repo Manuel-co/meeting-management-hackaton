a11e6d8c30e20f67631e54caf5f2ce1560b92167
Manuel-co <nwekeemmanuel2m@gmail.com>
2024-01-10 20:37:59 +0100

    fix-password

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -72,7 +72,6 @@ const page = () => {
 
             <div className="relative z-0 w-full mb-5 group">
               <input
-                type="password"
                 name="floating_password"
                 id="floating_password"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
